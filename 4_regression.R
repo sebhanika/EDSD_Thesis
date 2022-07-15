061bf5a84b296143e4b7c059ad1d06c2c28e5f5e
sebhanika <hanikasebastian@gmail.com>
2022-07-16 00:19:19 +0200

    MICE models Base

--- 4_regression.R
+++ 4_regression.R
@@ -24,58 +24,199 @@ options(scipen=999) # disable scientific notation
 
 
 
-
 # Query data --------------------------------------------------------------
 
-### loading imputation variabel from database, all tables from edsd schema
-# n <- 10 # copied from above
-# names_imp_calc <- paste0("imp_calc_", 1:n)
+## loading imputation variabel from database, all tables from edsd schema
+n <- 10 # copied from above
+names_imp_calc <- paste0("imp_calc_", 1:n)
+
+tbl_list_imp <- lapply(names_imp_calc, function(t) tbl(con, in_schema('edsd', t)))
+
+# collect tables to tibble and trim white spaces
+df_imps_calc <- lapply(tbl_list_imp, function(t) collect(t) %>%
+                    mutate(across(where(is.character), str_trim))) #sometimes postgresql adds whitespace?
+
+# set shorter names for tables
+df_imps_calc <- setNames(df_imps_calc, names_imp_calc)
+
+
+
+
+
+### loading test data
 # 
-# tbl_list_imp <- lapply(names_imp_calc, function(t) tbl(con, in_schema('edsd', t)))
+# try_data_tbl <- tbl(con, in_schema('edsd', 'imp_calc_1'))
+# try_imp_cal_1 <- try_data_tbl %>%
+#   collect() %>%
+#   mutate(across(where(is.character), str_trim))
+
 # 
-# # collect tables to tibble and trim white spaces
-# df_imps_calc <- lapply(tbl_list_imp, function(t) collect(t) %>% 
-#                     mutate(across(where(is.character), str_trim))) #sometimes postgresql adds whitespace?
+# # 2009 variables
+# try_imp_calc2009 <- try_imp_cal_1 %>% 
+#   filter(year == 2009) %>% 
+#   mutate(com_ratio09 = inbound_commuter-outbound_commuter,
+#          com_ratio_pop09 = (com_ratio09/workPop)*100,
+#          workPop_ln09 = log(workPop)) %>% 
+#   group_by(muni_key) %>% 
+#   mutate(yg_work_sh09 = share[type == "r" & age == "below25" & sex == "total"]) %>%
+#   ungroup() %>% 
+#   select(muni_key, yg_work_sh09, com_ratio_pop09, workPop_ln09) %>% 
+#   distinct()
 # 
-# # set shorter names for tables
-# df_imps_calc <- setNames(df_imps_calc, names_imp_calc)
 # 
+# ## reshape data frame
+# wide_df <- try_imp_cal_1 %>% 
+#   mutate(rs7 = as.factor(rs7)) %>% 
+#   filter(year == 2019) %>% 
+#   select(-c(value, grw, inbound_commuter, outbound_commuter, workPop)) %>% 
+#   pivot_wider(names_sep = '_', 
+#               values_from = c('grw_p', 'share', 'grw_sh'),
+#               names_from = c('sex', 'age', 'type')) %>% 
+#   left_join(try_imp_calc2009, by = 'muni_key')
 
 
-try_data_tbl <- tbl(con, in_schema('edsd', 'imp_calc_1'))
-try_imp_cal_1 <- try_data_tbl %>%
-  collect() %>%
-  mutate(across(where(is.character), str_trim))
+
+# Data Wrangeling ---------------------------------------------------------
+
+# select variables for 2009 values
+data_09 <- lapply(df_imps_calc, function(x) x %>%
+                      filter(year == 2009) %>% 
+                      mutate(com_ratio09 = inbound_commuter-outbound_commuter,
+                             com_ratio_pop09 = (com_ratio09/workPop)*100,
+                             workPop_ln09 = log(workPop)) %>% 
+                      group_by(muni_key) %>% 
+                      mutate(yg_work_sh09 = share[type == "r" & age == "below25" & sex == "total"]) %>%
+                      ungroup() %>% 
+                      select(muni_key, yg_work_sh09, com_ratio_pop09, workPop_ln09) %>% 
+                      distinct()
+                    )
+  
+# reshape dataframes to wide format
+data_wide <- lapply(df_imps_calc, function(x) x %>% 
+                        mutate(rs7 = as.factor(rs7)) %>% 
+                        filter(year == 2019) %>% 
+                        select(-c(value, grw, inbound_commuter, outbound_commuter, workPop)) %>% 
+                        pivot_wider(names_sep = '_', 
+                                    values_from = c('grw_p', 'share', 'grw_sh'),
+                                    names_from = c('sex', 'age', 'type')))
+
+## join 2009 dataframes to each corresponding data data frame in list
+df_models <- list() #empty list to store values in
+
+for (i in 1:10){
+  df_models[[i]] <- left_join(data_wide[[i]],
+                              data_09[[i]], 
+                              by = 'muni_key')
+  }
+                            
+# set names for models
+df_models_names  <-  paste0("df_model_", 1:10)
+df_models <- setNames(df_models, df_models_names)
 
 
 
+# Model Total Retirement Employment ---------------------------------------
 
-# 2009 variables
-try_imp_calc2009 <- try_imp_cal_1 %>% 
-  filter(year == 2009) %>% 
-  mutate(com_ratio09 = inbound_commuter-outbound_commuter,
-         com_ratio_pop09 = (com_ratio09/workPop)*100,
-         workPop_ln09 = log(workPop)) %>% 
-  group_by(muni_key) %>% 
-  mutate(yg_work_sh09 = share[type == "r" & age == "below25" & sex == "total"]) %>%
-  ungroup() %>% 
-  select(muni_key, yg_work_sh09, com_ratio_pop09, workPop_ln09) %>% 
-  distinct()
+# create base model with only rs7 and East-Germany as variables
+ls_models_base <- lapply(df_models, 
+                         function(x) lmrob(grw_sh_total_aboveRet_total ~ 
+                                             rs7 +
+                                             east_ger,
+                                           data = x, k.max	= 300)
+                      )
 
 
-## reshape data frame
-wide_df <- try_imp_cal_1 %>% 
-  mutate(rs7 = as.factor(rs7)) %>% 
-  filter(year == 2019) %>% 
-  select(-c(value, grw, inbound_commuter, outbound_commuter, workPop)) %>% 
-  pivot_wider(names_sep = '_', 
-              values_from = c('grw_p', 'share', 'grw_sh'),
-              names_from = c('sex', 'age', 'type')) %>% 
-  left_join(try_imp_calc2009, by = 'muni_key')
+ls_models_A1 <- lapply(df_models, 
+                      function(x) lmrob(grw_sh_total_aboveRet_total ~ 
+                                          hubdist100 + 
+                                          com_ratio_pop09 +
+                                          yg_work_sh09 +
+                                          rs7 +
+                                          east_ger,
+                                        data = x, k.max	= 300)
+                    )
 
+
+ls_models_A2 <- lapply(df_models, 
+                    function(x) lmrob(grw_sh_total_aboveRet_total ~ 
+                                        hubdist100 + 
+                                        com_ratio_pop09 +
+                                        yg_work_sh09 +
+                                        rs7 +
+                                        east_ger+
+                                        grw_p_total_total_r,
+                                      data = x, k.max	= 300)
+)
+
+
+# Pool results and summarize for base model
+models_base_pool <- pool(ls_models_base)
+models_base_sum <- summary(models_base_pool)
+
+# Pool results and summarize for Model A1
+models_A1_pool <- pool(ls_models_A1)
+models_A1_sum <- summary(models_A1_pool)
+
+# Pool results and summarize for Model A2
+models_A2_pool <- pool(ls_models_A2)
+models_A2_sum <- summary(models_A2_pool)
+
+# calculate GOFs (R squared)
+models_base_r2 <- pool.r.squared(models_base_pool, adjusted = F)
+models_A1_r2 <- pool.r.squared(models_A1_pool, adjusted = F)
+models_A2_r2 <- pool.r.squared(models_A2_pool, adjusted = F)
+
+
+# turn pooled BASE models into Texreg object
+model_base_texreg <- createTexreg(
+                    coef.names = as.character(models_base_sum$term), # names of coefficients 
+                    coef = models_base_sum$estimate,  # values of coef
+                    se = models_base_sum$std.error, # stds of coef
+                    pvalues = models_base_sum$p.value) # pvalues
+
+# turn pooled A1 models into Texreg object
+model_A1_texreg <- createTexreg(
+                    coef.names = as.character(models_A1_sum$term), # names of coefficients 
+                    coef = models_A1_sum$estimate,  # values of coef
+                    se = models_A1_sum$std.error, # stds of coef
+                    pvalues = models_A1_sum$p.value) # pvalues
+
+# turn pooled A2 models into Texreg object
+model_A2_texreg <- createTexreg(
+                    coef.names = as.character(models_A2_sum$term), # names of coefficients 
+                    coef = models_A2_sum$estimate,  # values of coef
+                    se = models_A2_sum$std.error, # stds of coef
+                    pvalues = models_A2_sum$p.value) # pvalues
+
+nobs.models <- nrow(drop_na(df_models[[1]]))
+
+# extract NObs and rsquares from the models
+gofs_A <- list("Num. Obs." = c(nobs.models, nobs.models, nobs.models),
+             "R^2" = c(models_base_r2[1], models_A1_r2[1], models_A2_r2[1]))
+
+
+model_A_labels <- c("Base", "A1", "A2")
+coef_A_labels <- c('Intercept' , 
+                 'Medium-sized city, urban',
+                 'Small town, urban', 'Central City, rural','Medium-sized city, rural',
+                 'Small town, rural', "West Germany", "Distance to large city",
+                 'Comuter Ratio 2009', 'Share of young workers', 'Employment Growth')
+
+screenreg(l = list(model_base_texreg, model_A1_texreg, model_A2_texreg),
+          custom.model.names = model_A_labels,
+          custom.coef.names = coef_A_labels,
+          custom.gof.rows = gofs_A,
+          include.nobs = F,
+          dcolumn = T,
+          digits = 3
+)
 
 
 ###### Models with ageing as dependet variable
+
+
+
+
 
 
 
